3b17bd5a17beb767342fbb4007f9c2f667df20d4
SantiagoMona <santimona1@gmail.com>
2024-06-02 21:10:17 -0500

    create git ignore

--- simulacro2/obj/Debug/net8.0/simulacro2.AssemblyInfo.cs
+++ simulacro2/obj/Debug/net8.0/simulacro2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("simulacro2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62d4fddefc9c2fdee6a166f8d95be48aa7ddabcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("simulacro2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("simulacro2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
